b04da9ae12ee2297d134bc3fe04a3d87571fbf5a
Dsopinka <96312744+Dsopinka@users.noreply.github.com>
2023-04-06 03:20:05 +0000

    updated slideshow

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(expressSession({
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-const PORT = 3000;
+const PORT = 5000;
 
 app.listen(PORT, () => {
   console.log("App listening on port ", PORT);
@@ -34,13 +34,6 @@ app.get("/profile", (req, res) => {
   res.render("profile", {user});
 });
 
-app.get("/faq", (req, res) => {
-  res.sendFile(path.resolve(__dirname + "/views/", "faq.html"));
-});
-
-app.get("/terms", (req, res) => {
-  res.sendFile(path.resolve(__dirname + "/views/", "terms.html"));
-});
 
 app.get("/slide-show", (req, res) => {
   res.render("slide-show");
--- public/js/slide-show.js
+++ public/js/slide-show.js
@@ -4,32 +4,63 @@ const $ = (selector) => document.querySelector(selector);
 
 let imageCounter = 0;
 let imageCache = [];
+let slideshowInterval;
 
 const caption = $("#caption");
 const mainImage = $("#main_image");
+const prevButton = $("#prev");
+const nextButton = $("#next");
 
-const preloadImages = () => {
-  const links = document.querySelectorAll("a");
-  let image;
+const startSlideshow = () => {
+    slideshowInterval = setInterval(swapImage, 4000);
+}
 
-  for (let link of links) {
-    image = new Image();
-    image.src = link.href;
-    image.alt = link.title;
-    imageCache.push(image);
-  }
+const stopSlideshow = () => {
+    clearInterval(slideshowInterval);
 }
 
 const swapImage = () => {
-  imageCounter = (imageCounter + 1) % imageCache.length;
+    imageCounter = (imageCounter +1) % imageCache.length;
 
-  mainImage.src = imageCache[imageCounter].src;
-  mainImage.alt = imageCache[imageCounter].alt;
+    mainImage.src = imageCache[imageCounter].src;
+    mainImage.alt = imageCache[imageCounter].alt;
 
-  caption.textContent = imageCache[imageCounter].alt;
+    caption.textContent = imageCache[imageCounter].alt;
 }
 
+const showImage = (index) => {
+    if (index < 0 || index >= imageCache.length) {
+        return;
+    }
+
+    imageCounter = index;
+    mainImage.src = imageCache[imageCounter].src;
+    mainImage.alt = imageCache[imageCounter].alt;
+    caption.textContent = imageCache[imageCounter].alt;
+}
+
+prevButton.addEventListener("click", () => {
+    stopSlideshow();
+    imageCounter = (imageCounter - 1 + imageCache.length) % imageCache.length;
+    showImage(imageCounter);
+});
+
+nextButton.addEventListener("click", () => {
+    stopSlideshow();
+    imageCounter = (imageCounter + 1) % imageCache.length;
+    showImage(imageCounter);
+});
+
 document.addEventListener("DOMContentLoaded", () => {
-  preloadImages();
-  setInterval(swapImage, 2000);
+    const links = document.querySelectorAll("#image_list a");
+    let image;
+
+    for (let link of links) {
+        image = new Image();
+        image.src = link.href;
+        image.alt = link.title;
+        imageCache.push(image);
+    }
+
+    startSlideshow();
 });
